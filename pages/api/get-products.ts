b267e33a22114ad4cb12a29411ae38b6eb41dfba
dlwnghd <alion0513@naver.com>
2023-09-22 17:11:03 +0900

    ✨ Feat : Prisma & PlanetScale을 활용한 DB 연결
    - 상품 추가 api 생성
    - schema.prisma 파일 생성
    - DB 연동하여 화면에 데이터 출력(Read)

---
+++ pages/api/get-products.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const prisma = new PrismaClient()
+
+async function getProducts() {
+  try {
+    const response = await prisma.products.findMany()
+    console.log(response)
+    return response
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+type Data = {
+  items?: any
+  message: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  try {
+    const products = await getProducts()
+    res.status(200).json({ items: products, message: 'Success get Items' })
+  } catch (error) {
+    res.status(400).json({ message: 'Failed get Items' })
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,26 @@ import { useEffect, useRef, useState } from 'react'
 
 export default function Home() {
   const inputRef = useRef<HTMLInputElement>(null)
+  // const [products, setProducts] = useState<
+  //   { id: string; properties: { id: string }[] }[]
+  // >([])
   const [products, setProducts] = useState<
-    { id: string; properties: { id: string }[] }[]
+    {
+      id: number
+      name: string
+      image_url: string
+      category_id: number
+      createdAt: string
+    }[]
   >([])
 
+  // useEffect(() => {
+  //   fetch('/api/get-items')
+  //     .then((res) => res.json())
+  //     .then((data) => setProducts(data.items))
+  // }, [])
   useEffect(() => {
-    fetch('/api/get-items')
+    fetch('/api/get-products')
       .then((res) => res.json())
       .then((data) => setProducts(data.items))
   }, [])
@@ -41,6 +55,13 @@ export default function Home() {
           {products &&
             products.map((item) => (
               <div key={item.id}>
+                {item.name}
+                <span>{item.createdAt}</span>
+              </div>
+            ))}
+          {/* {products &&
+            products.map((item) => (
+              <div key={item.id}>
                 {JSON.stringify(item)}
                 {item.properties &&
                   Object.entries(item.properties).map(([key, value]) => (
@@ -60,7 +81,7 @@ export default function Home() {
                 <br />
                 <br />
               </div>
-            ))}
+            ))} */}
         </div>
       </main>
     </>
